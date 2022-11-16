b67ce0de7e42b1530c078b45d28c3f5f6a4f723a
salifou-root163 <50051019+salifou-root163@users.noreply.github.com>
2022-11-16 17:33:27 +0100

    enable endpoint actuator/prometheus

--- my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
+++ my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableEurekaClient
+
 public class MyFinderApplication {
 
 	public static void main(String[] args) {
